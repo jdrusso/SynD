4778596fe86a6ee31a125292bfbdc2c625a8837e
John Russo <jdrusso@users.noreply.github.com>
2022-09-17 12:05:15 -0700

    Refactor to expose host/bucket parameters

--- synd/hosted.py
+++ synd/hosted.py
@@ -2,28 +2,28 @@ import minio
 from synd.models.base import BaseSynDModel
 from io import BytesIO
 
-model_host = "minios.jdrusso.dev"
-model_bucket = "models"
+MODEL_HOST = "minios.jdrusso.dev"
+MODEL_BUCKET = "models"
 
 
-def make_minio_client(access_key: str, secret_key: str) -> minio.Minio:
+def make_minio_client(access_key: str, secret_key: str, model_host: str = MODEL_HOST) -> minio.Minio:
     client = minio.Minio(model_host, access_key=access_key, secret_key=secret_key)
     return client
 
 
-def load_hosted_model(identifier: str, access_key: str, secret_key: str) -> BaseSynDModel:
+def load_hosted_model(identifier: str, access_key: str, secret_key: str, bucket: str = MODEL_BUCKET) -> BaseSynDModel:
 
     client = make_minio_client(access_key, secret_key)
-    model = client.get_object(model_bucket, identifier)
+    model = client.get_object(bucket, identifier)
 
     return model
 
 
-def upload_model(model, identifier: str, access_key: str, secret_key: str):
+def upload_model(model, identifier: str, access_key: str, secret_key: str, bucket: str = MODEL_BUCKET):
 
     client = make_minio_client(access_key, secret_key)
 
     serialized = model.serialize()
 
     with BytesIO(serialized) as serialized_bytes:
-        client.put_object(model_bucket, identifier, data=serialized_bytes, length=len(serialized))
+        client.put_object(bucket, identifier, data=serialized_bytes, length=len(serialized))

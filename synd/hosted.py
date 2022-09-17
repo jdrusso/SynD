24b312e6e479fdba166eab4e3378eff1c3e84491
John Russo <jdrusso@users.noreply.github.com>
2022-09-17 12:24:31 -0700

    Provide client to upload, instead of specify key/secret

--- synd/hosted.py
+++ synd/hosted.py
@@ -6,22 +6,19 @@ MODEL_HOST = "minios.jdrusso.dev"
 MODEL_BUCKET = "models"
 
 
-def make_minio_client(access_key: str, secret_key: str, model_host: str = MODEL_HOST) -> minio.Minio:
-    client = minio.Minio(model_host, access_key=access_key, secret_key=secret_key)
+def make_minio_client(access_key: str, secret_key: str, model_host: str = MODEL_HOST, **client_kwargs) -> minio.Minio:
+    client = minio.Minio(model_host, access_key=access_key, secret_key=secret_key, **client_kwargs)
     return client
 
 
-def load_hosted_model(identifier: str, access_key: str, secret_key: str, bucket: str = MODEL_BUCKET) -> BaseSynDModel:
+def load_hosted_model(identifier: str, client: minio.Minio, bucket: str = MODEL_BUCKET) -> BaseSynDModel:
 
-    client = make_minio_client(access_key, secret_key)
     model = client.get_object(bucket, identifier)
 
     return model
 
 
-def upload_model(model, identifier: str, access_key: str, secret_key: str, bucket: str = MODEL_BUCKET):
-
-    client = make_minio_client(access_key, secret_key)
+def upload_model(model, identifier: str, client: minio.Minio, bucket: str = MODEL_BUCKET):
 
     serialized = model.serialize()
 
